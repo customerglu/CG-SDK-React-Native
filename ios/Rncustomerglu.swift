7b0396c79aa9ef3ce42420f58d01390b171c2ecb
Himanshu Trehan <88142817+himCustomerGlu@users.noreply.github.com>
2024-05-09 14:39:21 +0530

    SDK-1.5.5

--- ios/Rncustomerglu.swift
+++ ios/Rncustomerglu.swift
@@ -90,7 +90,7 @@ class Rncustomerglu: RCTEventEmitter{
     func setPlatformAndSdkVersion(){
 
         CustomerGlu.app_platform="REACT_NATIVE"
-        CustomerGlu.sdk_version="1.5.3"
+        CustomerGlu.sdk_version="1.5.5"
     }
     
     override func startObserving() {
