93249cd6d4c7ab431fb50c14a67d6464e1c21acc
Himanshu Trehan <88142817+himCustomerGlu@users.noreply.github.com>
2024-12-10 11:23:23 +0530

    SDK-2.1.1

--- android/src/main/java/com/reactnativerncustomerglu/RncustomergluModule.java
+++ android/src/main/java/com/reactnativerncustomerglu/RncustomergluModule.java
@@ -93,7 +93,7 @@ public class RncustomergluModule extends ReactContextBaseJavaModule implements L
     private void setPlatformAndSdkVersion() {
         if (CustomerGlu.getInstance() != null) {
 
-            CustomerGlu.cg_sdk_version = "2.1.0";
+            CustomerGlu.cg_sdk_version = "2.1.1";
             CustomerGlu.cg_app_platform = "REACT_NATIVE";
         }
     }
